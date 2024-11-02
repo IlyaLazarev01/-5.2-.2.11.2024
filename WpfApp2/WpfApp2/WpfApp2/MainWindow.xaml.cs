43f7299b11ef5450cc7aecbee9dfbd787af6529d
IlyaLazarev01 <167057430+IlyaLazarev01@users.noreply.github.com>
2024-11-02 09:20:57 +0300

    Add files via upload

---
+++ WpfApp2/WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace YourNamespace // Замените на ваш реальный namespace
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordBox.Password;
+
+            // Здесь вы добавляете логику для аутентификации пользователя
+            MessageBox.Show($"Имя пользователя: {username}, Пароль: {password}"); // Примерный вывод
+        }
+    }
+}
